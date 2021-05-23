4fc1515edac614fee6e6c6a3a4f6b45941c240af
Avery J <64054360+averyj123@users.noreply.github.com>
2021-05-23 14:09:14 -0700

    Update simple_equations.py

--- simple_equations.py
+++ simple_equations.py
@@ -1,3 +1,4 @@
+#FINISHED (more can be added)
 #This will check if the entered equation is actually an equation.
 variables = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 def check_for_equation(equation):
